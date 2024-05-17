4072f48cac0ef8f7eff4501aab6d9cc41eb6e3e9
rubmanb <rubenm1386@gmail.com>
2024-05-17 16:34:24 +0200

    pipes done

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -22,8 +23,32 @@ export class UncommonPageComponent {
 
   //i18nPlural pipe
   public clientes: string[] = ['Rubén', 'Lily', 'Fernando', 'Román', 'Maria', 'Carla', 'Juan Carlos'];
+  public clientesMap = {
+    '=0': 'No tenemos ningún cliente esperando',
+    '=1': 'Actualmente tenemos 1 cliente esperando',
+    '=2': 'Actualmente tenemos 2 clientes esperando',
+    'other': 'Actualmente tenemos # clientes esperando'
+  }
 
   RemoveCustomer() {
     this.clientes.pop()
   }
+
+
+  // key value pipe
+  public person = {
+    name: 'Rubén',
+    city: 'Valencia'
+  }
+
+  //async pipe
+  public myObserrvableTimer = interval(2000).pipe(
+    tap( value => console.log("tap", value))
+  );
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+   setTimeout(() => {
+    resolve('Promesa activada');
+   }, 3500);
+  });
 }
